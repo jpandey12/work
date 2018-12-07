71e7ebf73de1741988bf18700795600860560789
jpandey12 <jpandey@calstatele.edu>
2018-12-06 18:53:52 -0800

    first commit

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/jayshreepandey/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/jayshreepandey/myapplication/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.jayshreepandey.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button3,button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button3 = (Button) findViewById(R.id.button3);
+        button2 = (Button) findViewById(R.id.button2);
+
+        textView = (TextView)findViewById(R.id.tv);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread thread=new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                        }
+                        catch(Exception e)
+                        {
+
+                        }
+                    }
+                });
+                textView.setText("Clicked");
+            }});
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("unclicked");
+            }
+        });
+        }
     }
-}
